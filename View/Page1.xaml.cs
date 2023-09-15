ddeb5d022b010473f83a6b1d732c1e7259080953
Marcuszz <102037920+Marcuszz@users.noreply.github.com>
2023-09-15 11:30:06 +0800

    Add files via upload

---
+++ View/Page1.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PDC06Module02.ViewModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC06Module02.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            BindingContext = new StudentViewModel();
+        }
+    }
+}
